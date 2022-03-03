b5d1b35014b9688d21dce5a0082c6ffe057731b3
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-03-03 15:44:34 +0100

    Ajout et modication CSS responsive Ajout Pages vs ami et vs ordi

---
+++ script/script-pages.js
@@ -0,0 +1,15 @@
+/* Page VS ORDI : bouton toogle switch
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      // do this
+      console.log('Checked');
+    } else {
+      // do that
+      console.log('Not checked');
+    }
+  });
+});
+ */
